2372a8ec2257631e2267ec3ea9b4ef2faee9cb22
LucasGutknecht <23124854+LucasGutknecht@users.noreply.github.com>
2026-02-05 22:48:12 -0300

    Add parser method.
    Starter the building of the parser method at the parser.rs file.a

---
+++ Math/src/parser.rs
@@ -0,0 +1,21 @@
+/* The parser file*/
+struct BindPower {
+    operator: char,
+    lbp: i32,
+    rbp: i32,
+}
+
+struct Parser {
+    position: usize,
+    tokens: Vec<ExpressionTokens>
+}
+/*
+parse_expression(min_bp){
+    
+}
+*/
+impl Parser {
+    fn new(tokens: Vec<ExpressionTokens>) -> Self {
+        Parser { position: 0, tokens }
+    }
+}
